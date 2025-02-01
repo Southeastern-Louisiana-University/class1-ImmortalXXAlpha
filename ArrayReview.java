bdc2428a3f7c6d170133599f2015668de046f4de
John Montz <john.montz@selu.edu>
2025-02-01 01:02:57 -0600

    First commit

---
+++ ArrayReview.java
@@ -0,0 +1,24 @@
+public class sumReview {
+    public static void main(String[] args) {
+        double doubleArray[];
+        doubleArray = new double[10];
+        System.out.println("Value of double [0] is doubleArray[0]");
+        System.out.println("Before");
+        sumArray (doubleArray);
+    for( int i=0; i< doubleArray.length; i++) {
+        doubleArray(1) = 1;
+    }
+        System.out.println("After")
+        sumArray(doubleArray)
+
+    }
+
+}
+private static double sumArray(double[] doubleArray) {
+    }
+
+
+}
+
+
+}
