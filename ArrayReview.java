8eafd20180b8ff55a7677dc5f6363db53c6f6c55
John Montz <john.montz@selu.edu>
2025-02-04 18:23:30 -0600

    Final upload

--- ArrayReview.java
+++ ArrayReview.java
@@ -1,24 +1,29 @@
-public class sumReview {
+public class ArrayReview {
     public static void main(String[] args) {
         double doubleArray[];
         doubleArray = new double[10];
         System.out.println("Value of double [0] is doubleArray[0]");
         System.out.println("Before");
-        sumArray (doubleArray);
-    for( int i=0; i< doubleArray.length; i++) {
-        doubleArray(1) = 1;
-    }
-        System.out.println("After")
-        sumArray(doubleArray)
+        printArray(doubleArray);
+        initToIndex(doubleArray);
+        sumArray(doubleArray);
+        System.out.println("After");
+        printArray(doubleArray);
 
     }
-
-}
-private static double sumArray(double[] doubleArray) {
+    public static void printArray(double[] doubleArray) {
+        for (int i = 0; i < doubleArray.length; i++) {
+            System.out.println(doubleArray[i]);
+        }
     }
-
-
-}
-
-
+    public static void initToIndex(double[] doubleArray) {
+        for (int i = 0; i < doubleArray.length; i++) {
+            doubleArray[i]= i;
+        }
+    }
+    public static void sumArray(double[] doubleArray) {
+        for (int i = 0; i < doubleArray.length; i++) {
+            doubleArray[i] += i;
+        }
+    }
 }
