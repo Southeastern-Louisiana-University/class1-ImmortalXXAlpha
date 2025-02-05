e884e49e8b6d1cc4b23b4b1f130b6d04be632dac
John Montz <john.montz@selu.edu>
2025-02-04 18:49:18 -0600

    First commit

---
+++ WhileLoops.java
@@ -0,0 +1,12 @@
+public class WhileLoops {
+    public static void main(String[] args){
+        /* Initialization */
+        int i =1;
+
+        while(i <= 5){
+            /* Condition */
+            System.out.println(i);
+            i++; //Increment
+        }
+    }
+}
